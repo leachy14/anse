5604f9dd9b5d17ffb691d330f96f7c7c40e30352
Christopher <leachy114@gmail.com>
2023-11-06 20:31:29 +0000

    added gpt4 turbo preview

--- src/providers/openai/index.ts
+++ src/providers/openai/index.ts
@@ -39,6 +39,7 @@ const providerOpenAI = () => {
           { value: 'gpt-3.5-turbo-0613', label: 'gpt-3.5-turbo-0613' },
           { value: 'gpt-3.5-turbo-16k', label: 'gpt-3.5-turbo-16k' },
           { value: 'gpt-3.5-turbo-16k-0613', label: 'gpt-3.5-turbo-16k-0613' },
+          { value: 'gpt-4-1106-preview', label: 'gpt-4-turbo-preview'}
         ],
         default: 'gpt-3.5-turbo',
       },
